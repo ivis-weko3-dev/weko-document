24cc92b047f14e9a3fbe0f96dc095b6e1f993fcd
Masaharu Hayashi <mhaya@nii.ac.jp>
2026-02-02 01:39:46 +0900

    fix

--- docs/build.sh
+++ docs/build.sh
@@ -7,6 +7,6 @@ mkdir -p build/spec
 mkdir -p build/user
 mkdir -p build/admin
 npx honkit build spec/base/ ../../build/spec/
-npx honkit build manual/USER/base ../../build/user
-npx honkit build manual/ADMIN/base ../../build/admin
+npx honkit build manuals/USER/base ../../build/user
+npx honkit build manuals/ADMIN/base ../../build/admin
 
