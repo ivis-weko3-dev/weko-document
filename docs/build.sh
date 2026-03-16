236d639ba87e3eedc41420a65549e208909c3d7f
Masaharu Hayashi <mhaya@nii.ac.jp>
2026-03-17 03:53:09 +0900

    update

--- docs/build.sh
+++ docs/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+rm -r build/user
 mkdir -p build/user/pdf
 mkdir -p build/user/html
 npx honkit pdf manuals/USER/base build/user/pdf/user.pdf
@@ -14,5 +15,17 @@ npx honkit build manuals/ADMIN/base build/admin/html
 rm -r build/spec
 mkdir -p build/spec/pdf
 mkdir -p build/spec/html
-npx honkit pdf spec/base build/spec/spec.pdf
+npx honkit pdf spec/base build/spec/pdf/spec.pdf
 npx honkit build spec/base build/spec/html
+
+rm -r build/未病User
+mkdir -p build/未病User/pdf
+mkdir -p build/未病User/html
+npx honkit pdf manuals/未病User/base build/未病User/pdf/未病User.pdf
+npx honkit build manuals/未病User/base build/未病User/html
+
+rm -r build/GUIDE
+mkdir -p build/GUIDE/pdf
+mkdir -p build/GUIDE/html
+npx honkit pdf manuals/GUIDE/base build/GUIDE/pdf/GUIDE.pdf
+npx honkit build manuals/GUIDE/base build/GUIDE/html
---
+++ docs/manuals/GUIDE/base/SUMMARY.md
@@ -0,0 +1,6 @@
+# 目次
+
+<!--## [JAIRO Cloud（WEKO3）登録ガイド　学術雑誌論文編](./journalPaperdeposit.md)-->
+<!--## [JAIRO Cloud（WEKO3）登録ガイド　研究データ編](./researchdatadeposit.md)-->
+## [GakuNin RDM-JAIRO Cloud連携を利用した場合の操作](./GJIntegration.md)
+## [OAアシストを利用した場合の操作](./OAAssist.md)
