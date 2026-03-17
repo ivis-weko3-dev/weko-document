9202f505319c781859ae0a1d0522db0b81ffba2b
Masaharu Hayashi <mhaya@nii.ac.jp>
2026-03-17 21:44:02 +0900

    update v2.0.0

--- docs/build.sh
+++ docs/build.sh
@@ -1,31 +1,36 @@
 #!/bin/bash
 
-rm -r build/user
-mkdir -p build/user/pdf
-mkdir -p build/user/html
-npx honkit pdf manuals/USER/base build/user/pdf/user.pdf
-npx honkit build manuals/USER/base build/user/html/
+# echo "build user manual"
+# rm -r build/user
+# mkdir -p build/user/pdf
+# mkdir -p build/user/html
+# npx honkit pdf manuals/USER/base build/user/pdf/user.pdf
+# npx honkit build manuals/USER/base ../../../build/user/html/
 
-rm -r build/admin
-mkdir -p build/admin/pdf
-mkdir -p build/admin/html
-npx honkit pdf manuals/ADMIN/base build/admin/pdf/admin.pdf
-npx honkit build manuals/ADMIN/base build/admin/html
+# echo "build admin manual"
+# rm -r build/admin
+# mkdir -p build/admin/pdf
+# mkdir -p build/admin/html
+# npx honkit pdf manuals/ADMIN/base build/admin/pdf/admin.pdf
+# npx honkit build manuals/ADMIN/base ../../../build/admin/html
 
+echo "build spec document"
 rm -r build/spec
 mkdir -p build/spec/pdf
 mkdir -p build/spec/html
 npx honkit pdf spec/base build/spec/pdf/spec.pdf
-npx honkit build spec/base build/spec/html
+npx honkit build spec/base ../../build/spec/html
 
-rm -r build/未病User
-mkdir -p build/未病User/pdf
-mkdir -p build/未病User/html
-npx honkit pdf manuals/未病User/base build/未病User/pdf/未病User.pdf
-npx honkit build manuals/未病User/base build/未病User/html
+# echo "build 未病USER manual"
+# rm -r build/未病USER
+# mkdir -p build/未病USER/pdf
+# mkdir -p build/未病USER/html
+# npx honkit pdf manuals/未病USER/base build/未病USER/pdf/未病USER.pdf
+# npx honkit build manuals/未病USER/base ../../../build/未病USER/html
 
-rm -r build/GUIDE
-mkdir -p build/GUIDE/pdf
-mkdir -p build/GUIDE/html
-npx honkit pdf manuals/GUIDE/base build/GUIDE/pdf/GUIDE.pdf
-npx honkit build manuals/GUIDE/base build/GUIDE/html
+# echo "build GUIDE manual"
+# rm -r build/GUIDE
+# mkdir -p build/GUIDE/pdf
+# mkdir -p build/GUIDE/html
+# npx honkit pdf manuals/GUIDE/base build/GUIDE/pdf/GUIDE.pdf
+# npx honkit build manuals/GUIDE/base ../../../build/GUIDE/html/
--- docs/manuals/GUIDE/base/SUMMARY.md
+++ docs/manuals/GUIDE/base/SUMMARY.md
@@ -1,6 +1,7 @@
 # 目次
 
-<!--## [JAIRO Cloud（WEKO3）登録ガイド　学術雑誌論文編](./journalPaperdeposit.md)-->
-<!--## [JAIRO Cloud（WEKO3）登録ガイド　研究データ編](./researchdatadeposit.md)-->
-## [GakuNin RDM-JAIRO Cloud連携を利用した場合の操作](./GJIntegration.md)
-## [OAアシストを利用した場合の操作](./OAAssist.md)
+## [操作の流れに沿ったマニュアル](./manual/README.md)
+* [JAIRO Cloud（WEKO3）登録ガイド　学術雑誌論文編](./manual/journalPaperdeposit.md)
+* [JAIRO Cloud（WEKO3）登録ガイド　研究データ編](./manual/researchdatadeposit.md)
+* [GakuNin RDMxJAIRO Cloud連携を利用した場合の操作](./manual/GJIntegration.md)
+* [OA Assistを利用した場合の操作](./manual/OAAssist.md)
